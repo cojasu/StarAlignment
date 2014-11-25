ae100c89aa25e909649800045452ede01d59a6af
cojasu <cjs110@zips.uakron.edu>
2014-11-25 15:45:41 -0500

    Added file opening.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StarAlignment
+{
+    public partial class Form1 : Form
+    {
+        List<Sequence> sequences = new List<Sequence>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream stream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((stream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (stream)
+                        {
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk.  Original error: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ Sequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarAlignment
+{
+    public class Sequence
+    {
+        string strand;
+
+        Sequence(string strand)
+        {
+            this.strand = strand;
+        }
+    }
+}
